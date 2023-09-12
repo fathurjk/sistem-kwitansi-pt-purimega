e00239cae877f02dc7eb6dbbbf12d9b1355a48e2
alfan <muhamadalfan12maret@gmail.com>
2023-09-12 09:33:58 +0700

    form kwitansi

---
+++ resources/views/templates/footer.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+
+<!--/.row -->
+        <hr class="mt-13 mt-md-15 mb-7" />
+        <div class="d-md-flex align-items-center justify-content-between">
+            <p style="text-align: center" class="mb-2 mb-lg-0">&copy; 2023 PT SATRIO MEGA SARANA. All rights reserved.</p>
+        </div>
+
+</html>
---
+++ resources/views/templates/header.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html class="no-js" lang="en">
+
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>PT SATRIO MEGA SARANA</title>
+<link href="logo mitra.jpg">
+
+<head style="text-align: center" class="mb-2 mb-lg-0"> 
+    
+    <div class="d-md-flex align-items-center justify-content-between">
+        <div style="text-align: center" class="image">
+            <img src="logo mitra.jpg" alt="image" height="100" width="100"><br/>
+        <h1>SATRIO MEGA SARANA</h1>
+        <br>GENERAL CONTRACTOR - DEVELOPER - PERDAGANGAN UMUM
+        <br>Jl. Setrayasa Barat 1 No. Kav 19 Kedungjaya Kedawung Cirebon 45135 Telp. (0231) 8807370
+    </div>
+    <!--/.row -->
+    <hr class="mt-13 mt-md-15 mb-7" />  
+    
+</head>
+</html>
