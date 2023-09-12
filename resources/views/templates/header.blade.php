9b8bcc576c74651242d498b3f23b2ab07004dabc
Fathurrochman JK <fathurrochmanjk@gmail.com>
2023-09-12 17:27:53 +0700

    header

--- resources/views/templates/header.blade.php
+++ resources/views/templates/header.blade.php
@@ -5,46 +5,61 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PT SATRIYO MEGA SARANA</title>
     <link href="logo mitra.jpg">
-
-</head>
-<body>
-    <div class="headerlogo">
-        <img src="logo mitra.jpg" alt="image">
-        <h1>PT. Satriyo Mega Sarana</h1>
-    </div>
-    <div class="headerkonten">
-        <p class="text">GENERAL CONTRACTOR - DEVELOPER - PERDAGANGAN UMUM
-        </p>
-        <p class="text">Jl. Setrayasa Barat 1 No. Kav 19 Kedungjaya Kedawung Cirebon 45135 Telp. (0231) 8807370</p>
+    <div class="containerlogo">
+        <div class="headerlogo">
+            <img src="logo mitra.jpg" alt="image">
+        </div>
+        <div class="headerkonten">
+            <h1>PT. Satriyo Mega Sarana</h1>
+            <p class="text">GENERAL CONTRACTOR - DEVELOPER - PERDAGANGAN UMUM
+            </p>
+            <p class="text">Jl. Setrayasa Barat 1 No. Kav 19 Kedungjaya Kedawung Cirebon 45135 Telp. (0231) 8807370</p>
+        </div>
     </div>
     <div class="divider"></div>
-</body>
+</head>
 </html>
 
 
 <style>
-    .headerlogo {
+    .containerlogo{
+        padding-top: 2rem;
         display: flex;
-        align-items: center;
-        text-align: center;
+        flex-direction: row;
         justify-content: center
     }
 
+    .headerlogo {
+        display: flex;
+        margin: 0;
+        padding: 0
+    }
+
     .headerlogo img {
-        height: 55px;
-        width: 60px;
-        margin-right: 10px;
+        height: 6rem;
+        width: 6rem;
+        margin-right: 8px;
+    }
+
+    .headerkonten h1 {
+        margin: 0;
+        padding: 0;
     }
 
     .headerkonten{
         justify-content: center;
         text-align: center;
+    }
+    
+    .headerkonten p {
         margin-top: 8px;
+        padding: 0
     }
     .divider {
         border-top: 3px solid;
-        margin: 18px 0; 
+        margin: 8px 0; 
     }
+
     .text {
         margin-bottom: 0;
         padding: 0;
