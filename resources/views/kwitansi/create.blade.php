005038ca1625a27284074f5793c8a7f015e6bb50
alfan <muhamadalfan12maret@gmail.com>
2023-09-19 09:34:23 +0700

    create

--- resources/views/kwitansi/create.blade.php
+++ resources/views/kwitansi/create.blade.php
@@ -5,7 +5,7 @@
     {{-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"> --}}
     <title>Tambah Kwitansi</title>
-    <a class="btn btn-primary mb-3" href="/kwitansi">Kembali</a>
+    
 </body>
 </head>
 
@@ -21,12 +21,16 @@
                             style="bottom: -1.8rem; right: -0.8rem; width: 85%; height: 90%;"></div>
                     </div>
                     <!-- welcome text -->
-                    <div class="title-form mt-3" id="title-form" style="text-align: center">
+                    
+                    <div class="title-form mt-3 mb-1" id="title-form" style="text-align: center">
+                        
                         <h1>TAMBAH KWITANSI</h1>
+
                     </div>
 
                     <form action="{{ route('kwitansi.store') }}" method="post" style="margin-bottom: 1%">
                         @csrf
+                        <a class="btn btn-primary mb-3" href="/kwitansi">Kembali</a>
                         <div style="text-align: right">
                             <div class="nomor">
                                 <label for="nomor_kwitansi">No :</label>
@@ -36,34 +40,34 @@
                         </div>
                         <div class="row mb-5 mt-5">
                             <label for="nama_lengkap" class="col-sm-2 col-form-label">Nama Lengkap</label>
-                            <div class="col-sm-10">
+                            <div class="col-sm-4">
                                 <input type="text" class="form-control" id="nama_lengkap" name="nama_lengkap" placeholder="Masukkan Nama Lengkap" required>
                             </div>
                         </div>
 
                         <div class="row mb-5">
                             <label for="alamat" class="col-sm-2 col-form-label">Alamat</label>
-                            <div class="col-sm-10">
+                            <div class="col-sm-4">
                                 <input type="text" class="form-control" id="alamat" name="alamat" placeholder="Masukkan Alamat" required>
                             </div>
                         </div>
 
                         <div class="row mb-5">
                             <label for="no_hp" class="col-sm-2 col-form-label"> No HP </label>
-                            <div class="col-sm-10">
+                            <div class="col-sm-4">
                                 <input id="no_hp" class="form-control" type="text" id="no_hp" name="no_hp" placeholder="Masukkan Nomor HP" required>
                             </div>
                         </div>
 
                         <div class="row mb-5">
                             <label for="uang_sebanyak" class="col-sm-2 col-form-label">Uang Sebanyak</label>
-                            <div class="col-sm-10">
+                            <div class="col-sm-4">
                                 <input class="form-control" id="uang_sebanyak" name="uang_sebanyak" placeholder="Masukkan Uang Sebanyak" required>
                             </div>
                         </div>
-                        <div class="row mb-4">
+                        <div class="row mb-5">
                             <label for="pembayaran" class="col-sm-2 col-form-label">Pembayaran</label>
-                            <div class="col-sm-10">
+                            <div class="col-sm-4">
                                 {{-- <div class="form-group mb-3">
                                     <select class="form-control" name="pembayaran" id="pembayaran">
                                         <option value="booking">Booking</option>
@@ -74,7 +78,7 @@
                                         <option value="lainlain">Lain-lain</option>
                                     </select>
                                 </div> --}}
-                                <input type="text" class="form-control" id="pembayaran" name="pembayaran"placeholder="Masukkan Peembayaran" required>
+                                <input type="text" class="form-control" id="pembayaran" name="pembayaran"placeholder="Masukkan Pembayaran" required>
                                 {{-- <div class="form-group mb-3" id="lain-lain-form" style="display: none;">
                                 </div> --}}
 
@@ -103,46 +107,46 @@
                         </div>
                         <fieldset style="margin-bottom: 1%">
                             <div>
-                                <div style="margin-bottom: 1%">
+                                <div class="row mb-5">
                                     <label for="lokasi" class="col-sm-2 col-form-label">Lokasi</label>
-                                    <div class="col-sm-10">
+                                    <div class="col-sm-4">
                                         <input class="form-control" id="lokasi" name="lokasi" placeholder="Masukkan Lokasi" required>
                                     </div>
                                 </div>
 
-                                <div style="margin-bottom: 1%">
+                                <div class="row mb-5" >
                                     <label for="no_kavling" class="col-sm-2 col-form-label">No. Kavling</label>
-                                    <div class="col-sm-10">
+                                    <div class="col-sm-4">
                                         <input class="form-control" id="no_kavling" name="no_kavling" placeholder="Masukkan Nomor Kavling" required>
                                     </div>
                                 </div>
 
-                                <div style="margin-bottom: 1%">
+                                <div class="row mb-5">
                                     <label for="type" class="col-sm-2 col-form-label">Type</label>
-                                    <div class="col-sm-10">
+                                    <div class="col-sm-4">
                                         <input class="form-control" id="type" name="type" placeholder="Masukkan Nama Type" required>
                                     </div>
                                 </div>
 
-                                <div style="margin-bottom: 1%">
+                                <div class="row mb-5">
                                     <label for="luas" class="col-sm-2 col-form-label">Luas</label>
-                                    <div class="col-sm-10">
+                                    <div class="col-sm-4">
                                         <input class="form-control" id="luas" name="luas" placeholder="Masukkan Luas" required>
                                     </div>
                                 </div>
 
-                                <div style="margin-bottom: 1%">
+                                <div class="row mb-5">
                                     <label for="discount" class="col-sm-2 col-form-label">Discount</label>
-                                    <div class="col-sm-10">
+                                    <div class="col-sm-4">
                                         <input class="form-control" id="discount" name="discount" placeholder="Masukkan Discount" required>
                                     </div>
                                 </div>
                             </div>
                         </fieldset>
 
-                        <div class="mb-5">
+                        <div class="row mb-5">
                             <label for="jumlah" class="col-sm-2 col-form-label">Jumlah</label>
-                            <div class="col-sm-10">
+                            <div class="col-sm-4">
                                 <input class="form-control" id="jumlah" name="jumlah" placeholder="Masukkan Jumlah" required>
                             </div>
                         </div>
