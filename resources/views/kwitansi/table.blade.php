e3d8d89ea4448ce7c4b42d5e928cf968aeeeee4b
Fathurrochman JK <fathurrochmanjk@gmail.com>
2023-09-25 20:22:11 +0700

    jk

--- resources/views/kwitansi/table.blade.php
+++ resources/views/kwitansi/table.blade.php
@@ -1,17 +1,17 @@
 <table class="table table-hover text-center">
     <thead>
         <tr class="bg-info">
-            <th>No</th>
-            <th>Nomor Kwitansi</th>
+            <th>No.</th>
+            <th>No. Kwitansi</th>
             <th>Nama Lengkap</th>
-            <th>Alamat Lengkap</th>
+            <th>Alamat</th>
             <th>No. HP</th>
             <th>Terbilang</th>
             <th>Pembayaran</th>
+            <th>Keterangan</th>
             <th>Nama Perumahan</th>
             <th>No. Kavling</th>
             <th>Type</th>
-            <th>Luas</th>
             <th>Jumlah</th>
         </tr>
     </thead>
@@ -25,10 +25,10 @@
                 <td>{{ $kwitansi->no_hp }}</td>
                 <td>{{ $kwitansi->terbilang }}</td>
                 <td>{{ $kwitansi->pembayaran }}</td>
+                <td>{{ $kwitansi->keterangan }}</td>
                 <td>{{ $kwitansi->lokasi }}</td>
                 <td>{{ $kwitansi->no_kavling }}</td>
                 <td>{{ $kwitansi->type }}</td>
-                <td>{{ $kwitansi->luas }}</td>
                 <td>{{ $kwitansi->jumlah }}</td>
             </tr>
         @endforeach
