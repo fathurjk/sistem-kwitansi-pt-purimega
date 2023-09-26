ee5136eb5863cc4c401652beff41c311b19ca9a8
Fathurrochman JK <fathurrochmanjk@gmail.com>
2023-09-26 14:21:18 +0700

    jk

--- resources/views/kwitansi/create.blade.php
+++ resources/views/kwitansi/create.blade.php
@@ -6,6 +6,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <title>Tambah Kwitansi</title>
+    <link rel="icon" href="{{ asset('img/logoremove.png') }}">
 </head>
 
 <body>
