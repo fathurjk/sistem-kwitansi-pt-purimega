8681559447860f0df460d4d9da4d8e05f79e23e1
Fathurrochman JK <fathurrochmanjk@gmail.com>
2023-09-29 12:15:41 +0700

    jk

--- resources/views/templates/sidebar.blade.php
+++ resources/views/templates/sidebar.blade.php
@@ -1,26 +1,29 @@
-<div class="offcanvas offcanvas-start" data-bs-scroll="true" tabindex="-1" id="offcanvasWithBothOptions" aria-labelledby="offcanvasWithBothOptionsLabel">
+<div class="offcanvas offcanvas-start" data-bs-scroll="true" tabindex="-1" id="offcanvasWithBothOptions"
+    aria-labelledby="offcanvasWithBothOptionsLabel">
     <div class="offcanvas-header">
         <h6 class="offcanvas-title d-none d-sm-block" id="offcanvas">Menu</h6>
         <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
     </div>
     <div class="offcanvas-body px-0">
         <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-start" id="menu">
-            <li class="nav-item">
-                <a href="{{ route('dashboard') }}" class="nav-link text-truncate">
-                    <i class="fs-5 bi-house"></i><span class="ms-1 d-none d-sm-inline">Dashboard</span>
-                </a>
-            </li>
+            @can('super admin')
+                <li class="nav-item">
+                    <a href="{{ route('dashboard') }}" class="nav-link text-truncate">
+                        <i class="fs-5 bi-house"></i><span class="ms-1 d-none d-sm-inline">Dashboard</span>
+                    </a>
+                </li>
+            @endcan
             <li>
                 <a href="{{ route('kwitansi') }}" class="nav-link text-truncate">
                     <i class="fs-5 bi-table"></i><span class="ms-1 d-none d-sm-inline">List Kwitansi</span></a>
             </li>
             @can('super admin')
-            <li>
-                <a href="{{ route('manage.users') }}" class="nav-link text-truncate">
-                    <i class="fs-5 bi-grid"></i><span class="ms-1 d-none d-sm-inline">Manage Admin</span></a>
-            </li>
+                <li>
+                    <a href="{{ route('manage.users') }}" class="nav-link text-truncate">
+                        <i class="fs-5 bi-grid"></i><span class="ms-1 d-none d-sm-inline">Manage Admin</span></a>
+                </li>
             @endcan
-            
+
         </ul>
     </div>
 </div>
