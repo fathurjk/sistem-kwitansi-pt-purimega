5000a749df9515839bab8bb79f0e52cda97249c8
Fathurrochman JK <fathurrochmanjk@gmail.com>
2023-09-29 20:19:19 +0700

    jk

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -108,15 +108,7 @@
         </div>
     </div>
 
-    <!-- Footer -->
-    <footer class="text-center text-lg-start">
-        <!-- Copyright -->
-        <div class="text-center p-3">
-            © 2023 Copyright:
-            <a class="text-dark text-decoration-none" href="https://tamananggrekgroup.co.id/">Taman Anggrek Group</a>
-        </div>
-        <!-- Copyright -->
-    </footer>
+    @extends('templates.footer')
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
